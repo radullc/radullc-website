4955fd4b89b60dc14d0b06a17c27a044e04d83d3
papucicumuci <47945947+papucicumuci@users.noreply.github.com>
2021-02-08 16:29:40 +0200

    Added PWA support

--- public/index.js
+++ public/index.js
@@ -1,2 +1,5 @@
 $('.preloader-back').delay(1000).fadeOut('slow')
 $('.progress').delay(1000).fadeOut('slow')
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('sw.js')
+}
--- server.js
+++ server.js
@@ -7,12 +7,17 @@ let key;
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.get('/', (req, res)=>{
+    if(req.protocol==="http"){
+        res.redirect(`https://${req.headers.host}${req.url}`)
+    } 
+    else 
     res.render('index')
 })
 app.use((req, res)=>{
     res.render('error/404')
 })
 
+const https=require('https').createServer(app)
 try {
     cert=readF('certificate.pem', 'utf8')
     key=readF('key.pem', 'utf8')
@@ -22,4 +27,5 @@ try {
     console.log("Could not read certificate")
 }
 
+https.listen(443)
 http.listen(80)
