e999f4584426f6f46ec3f1f201229c1210ef456e
papucicumuci <47945947+papucicumuci@users.noreply.github.com>
2021-02-09 20:55:06 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', e=>{
     e.waitUntil(
         caches.open('static').then(cache=>{
-            return cache.addAll(['/', '/repos', '/main.css', '/radullc.png', '/materialize/css/materializemin.css', '/jquery.js', '/index.js', '/manifest.json', '/repos.js', '/repo.js', '/libs/request.js'])
+            return cache.addAll(['/', '/repos', '/main.css', '/radullc.png', '/materialize/css/materializemin.css', '/jquery.js', '/index.js', '/manifest.json', '/repos.js', '/repo.js', '/libs/request.js', '/repos.json'])
         })
     )
 })
