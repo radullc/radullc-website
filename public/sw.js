66749bd3ed471c103582471c63f39130ba0edf64
papucicumuci <47945947+papucicumuci@users.noreply.github.com>
2021-02-09 22:09:30 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', e=>{
 self.addEventListener('fetch', e=>{
     e.respondWith(
         caches.match(e.request).then(response=>{
-            return fetch(e.request) || response
+            return response || fetch(e.request)
         })
     )
 })
